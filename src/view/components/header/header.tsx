7d7265b1bd4be1f0e0e592e4eded81cc35d0903e
Aman <aman_A00@mail.ru>
2021-06-01 15:12:46 +0600

    header fix & welcome block

--- src/view/components/header/header.tsx
+++ src/view/components/header/header.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import Css from "./header.module.css";
 import { useState } from "react";
 import MenuBurger from "../menuBurger/menuBurger";
@@ -13,24 +13,26 @@ const Header = ({ isAuth }: PropsType) => {
   return (
     <header>
       <MenuBurger open={open} setOpen={setOpen} />
-      <Link className={Css.logo_wrapper} to="/">
-        <img className={Css.logo} src="./img/logo1.png" alt="logo1" />
-      </Link>
       <div className={Css.header_burger} onClick={() => setOpen(!open)}>
-        <span></span>
+        <span />
       </div>
-      <div className={Css.header_menu}>
-        <Link to="/news">
-          <div>Жаңылыктар</div>
+      <div className={Css.menu_wrapper}>
+        <Link className={Css.logo_wrapper} to="/">
+          <img className={Css.logo} src="./img/logo1.png" alt="logo1" />
         </Link>
-        <div className={Css.item}>Маектер</div>
-        <div className={Css.item}>Жарнамалар</div>
-        <div className={Css.item}>Байланыш</div>
-      </div>
-      <div className={Css.menu}>
-        <img className={Css.flag} src="./img/flag.png" alt="flag" />
-        <div className={Css.lang}>KG</div>
-        <img className={Css.vector} src="./img/vec.png" alt="vector" />
+        <div className={Css.header_menu}>
+          <NavLink activeClassName={Css.active} className={Css.item} to="/news">
+            Жаңылыктар
+          </NavLink>
+          <div className={Css.item}>Маектер</div>
+          <div className={Css.item}>Жарнамалар</div>
+          <div className={Css.item}>Байланыш</div>
+        </div>
+        <div className={Css.menu}>
+          <img className={Css.flag} src="./img/flag.png" alt="flag" />
+          <div className={Css.lang}>KG</div>
+          <img className={Css.vector} src="./img/vec.png" alt="vector" />
+        </div>
       </div>
       {!isAuth ? (
         <Link to="/authentication">
