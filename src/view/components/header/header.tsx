30e3c2647cb247b2556d018a169b34df937e190c
KojegulovB <75375116+KojegulovB@users.noreply.github.com>
2021-06-29 16:17:51 +0600

    locales-header

--- src/view/components/header/header.tsx
+++ src/view/components/header/header.tsx
@@ -2,16 +2,48 @@ import { Link, NavLink } from "react-router-dom";
 import Css from "./header.module.css";
 import { useState } from "react";
 import MenuBurger from "../menuBurger/menuBurger";
+import i18n from "../../../i18next/i18n";
+import { useTranslation } from "react-i18next";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import { makeStyles } from "@material-ui/core/styles";
+import "./select.css";
 
 interface PropsType {
   isAuth: boolean;
   name: string | null;
   img: string | undefined;
 }
+const useStyles = makeStyles((theme) => ({
+  select: {
+    color: "#FFFFFF",
+  },
+}));
 
 const Header = ({ isAuth, img, name }: PropsType) => {
   const [open, setOpen] = useState(false);
-
+  const [openSel, setOpenSel] = useState(false);
+  const [lan, setLan] = useState<any>(() => localStorage.getItem("i18nextLng"));
+  const { t } = useTranslation();
+  const onChange = (event: any) => {
+    i18n.changeLanguage(event.target.value);
+    setLan(event.target.value);
+    localStorage.setItem("i18nextLng", event.target.value);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleCloseSelect = () => {
+    setOpenSel(false);
+  };
+  const handleOpenSelect = () => {
+    setOpenSel(true);
+  };
+  const classes = useStyles();
   return (
     <header>
       <MenuBurger open={open} setOpen={setOpen} />
@@ -24,29 +56,58 @@ const Header = ({ isAuth, img, name }: PropsType) => {
         </Link>
         <div className={Css.header_menu}>
           <NavLink activeClassName={Css.active} className={Css.item} to="/news">
-            Жаңылыктар
+            {t("header.news")}
           </NavLink>
           <NavLink activeClassName={Css.active} className={Css.item} to="/ads">
-            Жарнамалар
+            {t("header.chatskg")}
           </NavLink>
           <NavLink
             activeClassName={Css.active}
             className={Css.item}
             to="/communication"
           >
-            Маектер
+            {t("header.posts")}
           </NavLink>
-          <div className={Css.item}>Байланыш</div>
+          <div className={Css.item}>{t("header.contacts")}</div>
         </div>
         <div className={Css.menu}>
-          <img className={Css.flag} src="/img/flag.png" alt="flag" />
-          <div className={Css.lang}>KG</div>
-          <img className={Css.vector} src="/img/vec.png" alt="vector" />
+          <FormControl>
+            <Select
+              labelId="demo-controlled-open-select-label"
+              id="demo-controlled-open-select"
+              open={openSel}
+              onClose={handleCloseSelect}
+              onOpen={handleOpenSelect}
+              value={lan}
+              onChange={onChange}
+              className={classes.select}
+              disableUnderline
+            >
+              <MenuItem value="RU" className={Css.lan}>
+                <div className={Css.box_option}>
+                  <img src="/img/flag_ru.svg" className={Css.flag} />
+                  RU
+                </div>
+              </MenuItem>
+              <MenuItem value="KG" className={Css.lan}>
+                <div className={Css.box_option}>
+                  <img src="/img/flag_kg.svg" className={Css.flag} />
+                  KG
+                </div>
+              </MenuItem>
+              <MenuItem value="EN" className={Css.lan}>
+                <div className={Css.box_option}>
+                  <img src="/img/flag_en.svg" className={Css.flag} />
+                  En
+                </div>
+              </MenuItem>
+            </Select>
+          </FormControl>
         </div>
       </div>
       {!isAuth ? (
         <Link to="/authentication">
-          <button className={Css.btn}>КИРҮҮ</button>
+          <button className={Css.btn}>{t("header.login")}</button>
         </Link>
       ) : (
         <Link to="/my-profile">
